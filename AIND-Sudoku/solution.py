e4d4e6ed28bcb3c27468e73df5248eb7f86af169
stubz <yamamoto.yusuke+github@gmail.com>
2017-05-13 15:44:25 +0900

    made solution for the first project

--- AIND-Sudoku/solution.py
+++ AIND-Sudoku/solution.py
@@ -15,6 +15,23 @@ def assign_value(values, box, value):
         assignments.append(values.copy())
     return values
 
+def cross(A, B):
+    "Cross product of elements in A and elements in B."
+    return [s+t for s in A for t in B]
+
+rows = 'ABCDEFGHI'
+cols = '123456789'
+boxes = cross(rows, cols)
+
+row_units = [cross(r, cols) for r in rows]
+column_units = [cross(rows, c) for c in cols]
+square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
+diagonal_units = [['A1','B2','C3','D4','E5','F6','G7','H8','I9'],['A9','B8','C7','D6','E5','F4','G3','H2','I1']]
+unitlist = row_units + column_units + square_units + diagonal_units
+units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
+peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
+
+
 def naked_twins(values):
     """Eliminate values using the naked twins strategy.
     Args:
@@ -25,11 +42,34 @@ def naked_twins(values):
     """
 
     # Find all instances of naked twins
+    # find all possible combinations that can be twins
+    # First, find a list of cells that have two values
+    x1 = [box for box in values.keys() if len(values[box])==2]
+    twin1 = [(a,b) for i,a in enumerate(x1) for b in x1[i:] if (values[a]==values[b] and a!=b and a in peers[b] )]
+    # For each possible twins, check if they are in the same unit.
+    """
+    twins = {}
+    for twin in twin1:
+        if twin[0] in twins.keys():
+            twins[twin[0]].append(twin[1])
+        else:
+            twins[twin[0]] = []
+            twins[twin[0]].append(twin[1])
+    """
+    for t1, t2 in twin1:
+        # find units to which both of (t1, t2) in twin1 belong.
+        for unit in units[t1]:
+            if t2 in unit:
+                # replace values in the same unit
+                for u in unit:
+                    # one twin have two values, so we iterate two values one by one
+                    # ignore if the cell has only one value, or the cell is a twin.
+                    if len(values[u])==1 or u in (t1, t2):
+                        continue
+                    for digit in values[t1]:
+                        values[u] = values[u].replace(digit, '')
+    return values
     # Eliminate the naked twins as possibilities for their peers
-
-def cross(A, B):
-    "Cross product of elements in A and elements in B."
-    pass
 
 def grid_values(grid):
     """
@@ -41,7 +81,15 @@ def grid_values(grid):
             Keys: The boxes, e.g., 'A1'
             Values: The value in each box, e.g., '8'. If the box has no value, then the value will be '123456789'.
     """
-    pass
+    values = []
+    all_digits = '123456789'
+    for c in grid:
+        if c == '.':
+            values.append(all_digits)
+        elif c in all_digits:
+            values.append(c)
+    assert len(values) == 81
+    return dict(zip(boxes, values))
 
 def display(values):
     """
@@ -49,19 +97,81 @@ def display(values):
     Args:
         values(dict): The sudoku in dictionary form
     """
-    pass
+    width = 1+max(len(values[s]) for s in boxes)
+    line = '+'.join(['-'*(width*3)]*3)
+    for r in rows:
+        print(''.join(values[r+c].center(width)+('|' if c in '36' else '')
+                      for c in cols))
+        if r in 'CF': print(line)
+    return
 
 def eliminate(values):
-    pass
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
+    for box in solved_values:
+        digit = values[box]
+        for peer in peers[box]:
+            values[peer] = values[peer].replace(digit,'')
+    return values
 
 def only_choice(values):
-    pass
+    for unit in unitlist:
+        for digit in '123456789':
+            # for a given digit, return a list of cells that has the digit for each unit.
+            # e.g. for a unit of the first row ['A1','A2',...,'A9'], check which cells can have digit 1.
+            # If the length of this list is one, it means 1 can be in that particular cell in the unit.
+            dplaces = [box for box in unit if digit in values[box]]
+            if len(dplaces) == 1:
+                values[dplaces[0]] = digit
+    return values
 
 def reduce_puzzle(values):
-    pass
+    """
+    Iterate eliminate() and only_choice(). If at some point, there is a box with no available values, return False.
+    If the sudoku is solved, return the sudoku.
+    If after an iteration of both functions, the sudoku remains the same, return the sudoku.
+    Input: A sudoku in dictionary form.
+    Output: The resulting sudoku in dictionary form.
+    """
+    stalled = False
+    while not stalled:
+        # Check how many boxes have a determined value
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+        # Use the Eliminate Strategy
+        values = eliminate(values)
+        # Use the Only Choice Strategy
+        values = only_choice(values)
+        # Use the naked_twins Strategy
+        values = naked_twins(values)
+
+        # Check how many boxes have a determined value, to compare
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        # If no new values were added, stop the loop.
+        stalled = solved_values_before == solved_values_after
+        # Sanity check, return False if there is a box with zero available values:
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+    return values
 
 def search(values):
-    pass
+    "Using depth-first search and propagation, try all possible values."
+    # First, reduce the puzzle using the previous function
+    values = reduce_puzzle(values)
+    if values is False:
+        return False ## Failed earlier
+    # check if all the cells has only one value. If so, it means the sudoku was solved.
+    if all(len(values[s]) == 1 for s in boxes):
+        return values ## Solved!
+    # Choose one of the unfilled squares with the fewest possibilities
+    # note: min function takes two arguments (iterable, key). When the key is missing, it sorts
+    # by the first element. So the following returns a tuple that has the minimum length.
+    n,s = min((len(values[s]), s) for s in boxes if len(values[s]) > 1)
+    # Now use recurrence to solve each one of the resulting sudokus, and
+    for value in values[s]:
+        new_sudoku = values.copy()
+        new_sudoku[s] = value
+        attempt = search(new_sudoku)
+        if attempt:
+            return attempt
 
 def solve(grid):
     """
@@ -72,6 +182,8 @@ def solve(grid):
     Returns:
         The dictionary representation of the final sudoku grid. False if no solution exists.
     """
+    values = grid_values(grid)
+    return search(values)
 
 if __name__ == '__main__':
     diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
