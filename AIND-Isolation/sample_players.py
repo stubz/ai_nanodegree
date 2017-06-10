d82534b686c0fcdac3867671df664220f6613527
stubz <yamamoto.yusuke+github@gmail.com>
2017-06-10 12:02:48 +0900

    added AlphaBetaPlayer as player2

--- AIND-Isolation/sample_players.py
+++ AIND-Isolation/sample_players.py
@@ -7,7 +7,7 @@ own agent and example heuristic functions.
 """
 
 from random import randint
-
+from game_agent import *
 
 def null_score(game, player):
     """This heuristic presumes no knowledge for non-terminal states, and
@@ -256,8 +256,9 @@ if __name__ == "__main__":
 
     # create an isolation board (by default 7x7)
     player1 = RandomPlayer()
-    player2 = GreedyPlayer()
+    # player2 = GreedyPlayer()
     # player2 = MinimaxPlayer()
+    player2 = AlphaBetaPlayer()
     game = Board(player1, player2)
 
     # place player 1 on the board at row 2, column 3, then place player 2 on
