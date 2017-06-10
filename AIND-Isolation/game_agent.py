ac5c2ae30b861ff46a7739b93cd32456910cc9e1
stubz <yamamoto.yusuke+github@gmail.com>
2017-06-10 11:36:37 +0900

    copied open_move_score function to custom_score

--- AIND-Isolation/game_agent.py
+++ AIND-Isolation/game_agent.py
@@ -35,7 +35,15 @@ def custom_score(game, player):
         The heuristic value of the current game state to the specified player.
     """
     # TODO: finish this function!
-    raise NotImplementedError
+    if game.is_loser(player):
+        return float("-inf")
+
+    if game.is_winner(player):
+        return float("inf")
+
+    return float(len(game.get_legal_moves(player)))
+
+    #raise NotImplementedError
 
 
 def custom_score_2(game, player):
